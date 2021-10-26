d0dcda56e17e0246cdef3edd5d800258faa7ed50
Pablo Souza <cappuccinocodes@gmail.com>
2021-10-27 06:53:51 +1000

    Add project files.

---
+++ DatabaseManager.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTracker1
+{
+    class DatabaseManager
+    {
+        public static void CreateDatabase(string path)
+        {
+            File.Create("C:\\Projects\\Tutorials\\CodeTracker\\CodeTracker1\\db4.sqlite");
+            Console.WriteLine("Database Created");
+
+            using (var connection = new SqliteConnection("Data Source=db4.sqlite"))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText = "CREATE TABLE Coding ('Id' INTEGER,'Date'INTEGER 'Duration'  INTEGER); ";
+                tableCmd.ExecuteNonQuery();
+                Console.WriteLine("Table Created");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.IO;
+
+namespace CodeTracker1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Projects\Tutorials\CodeTracker\CodeTracker1\d4.sqlite";
+
+            //bool dbExists = File.Exists(path);
+            
+
+            if (true)
+                //Console.WriteLine("Database Exists");
+                using (var connection = new SqliteConnection("Data Source=C:\\Projects\\Tutorials\\CodeTracker\\CodeTracker1\\d4.sqlite"))
+                {
+                   
+                        connection.Open();
+                        var tableCmd = connection.CreateCommand();
+                        tableCmd.CommandText = "create table bolas (name varchar(20), score int) ";
+                        tableCmd.ExecuteNonQuery();
+                        connection.Close();
+                 
+                    
+                    Console.WriteLine("Table Created");
+                }
+            else
+                DatabaseManager.CreateDatabase(path);
+            return;
+        }
+    }
+}
